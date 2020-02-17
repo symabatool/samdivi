fa1ce08fdca68636328d03d8564edcc01b2322ae
TahaaFatima <tahafatima95@gmail.com>
2020-02-17 13:21:31 +0500

    added new files

---
+++ wp-content/themes/Divi-Child/template/findus.php
@@ -0,0 +1,24 @@
+<?php
+/*Template Name:custom Findus*/
+
+get_header();
+
+?>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.546878452634!2d67.05700095030568!3d24.879320983966732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33eed775959a5%3A0x653a6ae6b3f6a391!2sGenetech%20Solutions!5e0!3m2!1sen!2s!4v1581926336385!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<main>
+<section><div class="container">
+<h1>hello</h1>
+</div>
+<div class="row">
+<?php echo the title();?>
+</div>
+<div class="row">
+<?php echo the content();?>
+</div>
+
+</section>
+</main>
+<?php
+get_footer();
+?>
+
